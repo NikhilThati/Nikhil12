aa79b4533d72aaf8d827e70c6405c7af0f76d63a
NikhilThati <93723309+NikhilThati@users.noreply.github.com>
2022-01-03 20:38:08 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "myec2" {
   instance_type = "t2.micro"
   subnet_id   = "subnet-01b6ac774a28996b1"
   key_name = "Nikhil"
-  security_groups = ["launch-wizard-1"]
+  security_groups = ["sg-03fe3a9e93481754c"]
   tags = {
     Name = "Ec2tf"
   }
